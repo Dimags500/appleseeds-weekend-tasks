298dc5f273fb876519ae49ac5f909d5a9a19d706
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-15 17:28:36 +0300

    5.5

---
+++ week4/task1/JS questions/5.5.js
@@ -0,0 +1,18 @@
+// Ex5.5 - Abbreviate two words
+// Write a function to convert a name into initials. This kata strictly takes two words with one space
+// in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// Patrick Feeney => P.F
+
+const ntoInitials = (fullname) => {
+  let name_arr = fullname.split(" ");
+  let initials = name_arr.map((item) => {
+    return item.charAt(0).toUpperCase();
+  });
+
+  return initials.join(".");
+};
+
+console.log(ntoInitials("Patrick Feeney"));
