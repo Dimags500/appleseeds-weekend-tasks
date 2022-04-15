38f03e88f366b33c6e14d26c9ac508b41ee0b481
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-15 12:02:34 +0300

    2.4

---
+++ week4/task1/JS questions/2.4.js
@@ -0,0 +1,12 @@
+// Ex2.4 - Unique
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+function findUniq(array) {
+  return array.sort((a, b) => b - a).find((num, i) => num[i - 1] != num);
+}
+
+const resalut = findUniq([1, 1, 1, 2, 1, 1]);
+console.log(resalut);
