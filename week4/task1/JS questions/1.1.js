fb54b28987aaa1973a118c4bf5f277836c3d73a7
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-15 12:00:47 +0300

    1.1

---
+++ week4/task1/JS questions/1.1.js
@@ -0,0 +1,9 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No"
+// string for false.
+
+function yesOrNo(bool_value) {
+  return bool_value ? "Yes" : "No";
+}
+
+const res = yesOrNo(false);
+console.log(res);
