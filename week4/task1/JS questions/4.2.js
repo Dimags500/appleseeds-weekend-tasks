a516969bcbfe856294b416568997f6df94defb2d
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-17 06:28:12 +0300

    4.2 - Tribonacci  array solution

--- week4/task1/JS questions/4.2.js
+++ week4/task1/JS questions/4.2.js
@@ -1,17 +1,38 @@
-function trib(n) {
-  let n1 = 1,
-    n2 = 1,
-    n3 = 1,
-    nextNum = n1 + n2 + n3;
+// function trib(n) {
+//   let n1 = 1,
+//     n2 = 1,
+//     n3 = 1,
+//     nextNum = n1 + n2 + n3;
 
-  while (nextNum <= n) {
-    console.log(nextNum);
-    n1 = n2;
-    n2 = n3;
-    n3 = nextNum;
-    nextNum = n1 + n2 + n3;
+//   while (nextNum <= n) {
+//     console.log(nextNum);
+//     n1 = n2;
+//     n2 = n3;
+//     n3 = nextNum;
+//     nextNum = n1 + n2 + n3;
+//   }
+// }
+
+// trib(60);
+////////////////////////////??????????????????????????
+
+const signature = [0, 1, 1];
+function trib2(n, signature) {
+  if (n < 0) {
+    return [];
   }
+  let sequence = [...signature];
+
+  for (let i = 0; i < n - signature.length; i++) {
+    sequence.push(
+      sequence[sequence.length - 1] +
+        sequence[sequence.length - 2] +
+        sequence[sequence.length - 3]
+    );
+  }
+
+  console.log(sequence.slice(0, n));
+  return sequence.slice(0, n);
 }
 
-trib(6);
-////////////////////////////??????????????????????????
+trib2(10, signature);
