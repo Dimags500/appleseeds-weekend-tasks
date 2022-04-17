1773a799abcb4521230186cc3d5a111243a19f22
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-17 13:33:24 +0300

    recurfibonacci sequence using recursion

--- week4/task1/JS questions/4.1.js
+++ week4/task1/JS questions/4.1.js
@@ -1,14 +1,31 @@
-function fib(n) {
-  let n1 = 0,
-    n2 = 1,
-    nextNum = n1 + n2;
+// function fib(n) {
+//   let n1 = 0,
+//     n2 = 1,
+//     nextNum = n1 + n2;
 
-  while (nextNum <= n) {
-    console.log(nextNum);
-    n1 = n2;
-    n2 = nextNum;
-    nextNum = n1 + n2;
+//   while (nextNum <= n) {
+//     console.log("-----------------");
+//     console.log(n1);
+//     console.log(n2);
+//     console.log(nextNum);
+//     console.log("-----------------");
+
+//     n1 = n2;
+//     n2 = nextNum;
+//     nextNum = n1 + n2;
+//   }
+// }
+
+// fib(10);
+
+// program to display fibonacci sequence using recursion
+function fibonacci(num) {
+  if (num < 2) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
   }
 }
 
-fib(10);
+console.log(fibonacci(4));
+console.log(fibonacci(10));
