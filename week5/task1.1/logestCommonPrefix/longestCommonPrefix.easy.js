998b3cd3882bc6991a263defc689a53757a9dc17
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-21 10:34:33 +0300

    bad

--- week5/task1.1/logestCommonPrefix/longestCommonPrefix.easy.js
+++ week5/task1.1/logestCommonPrefix/longestCommonPrefix.easy.js
@@ -16,6 +16,25 @@
  *
  */
 
-const longestCommonPrefix = function (strs) {};
+const longestCommonPrefix = function (strs) {
+  const prefixArray = strs
+    .slice()
+    .sort()
+    .map((i) => (i = i[0] + i[1]));
+
+  let prefixObj = {};
+
+  prefixArray.forEach((item) => {
+    if (!prefixObj[item]) {
+      prefixObj[item] = 0;
+    }
+
+    prefixObj[item] += 1;
+  });
+
+  let superGo = Object.entries(prefixObj).sort((a, b) => b[1] - a[1]);
+
+  return superGo[0][1] > 1 ? superGo[0][0] : "";
+};
 
 module.exports = longestCommonPrefix;
