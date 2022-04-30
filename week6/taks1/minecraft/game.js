0369d371b5929473e428dcc9027a41eb8f3d65f2
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-30 15:08:28 +0300

    tool-1 full working

--- week6/taks1/minecraft/game.js
+++ week6/taks1/minecraft/game.js
@@ -2,20 +2,21 @@ import { boardArray } from "./board.js";
 
 let board = document.getElementById("game-board");
 let bank = document.getElementById("bank");
-const NADA = "1";
-bank.setAttribute("bank_value", NADA);
+const EMPTY = "0";
+bank.setAttribute("bank_value", EMPTY);
 
-let currTool;
+let currTool = "0";
 let tool1 = document.getElementById("tool1");
-let tool2 = document.getElementById("tool1");
-let tool3 = document.getElementById("tool1");
+let tool2 = document.getElementById("tool2");
+let tool3 = document.getElementById("tool3");
 let tools = [tool1, tool2, tool3].forEach((item) =>
   item.addEventListener("click", setTool)
 );
 
-function setTool(e) {}
-
-cellsBuilder(board);
+function setTool(e) {
+  currTool = e.target.getAttribute("tool-type");
+  console.log(currTool + " curr tool ");
+}
 
 function cellsBuilder(board) {
   for (let i = 0; i < 20; i++) {
@@ -51,36 +52,86 @@ function cellsBuilder(board) {
 }
 
 function cellClick(e) {
-  let currClass = e.target.classList[1];
-  let cellIndex = e.target.getAttribute("cell-index").split(",");
-  let cellType = e.target.getAttribute("cell-type");
+  switch (currTool) {
+    case undefined || "0":
+      alert("chose tool");
+      return;
+    case "1":
+      toolAction1(e);
+      return;
+    case 2:
+      toolAction2(e);
+      return;
+    case 3:
+      toolAction3(e);
+      return;
+  }
+}
 
+function toolAction1(e) {
   let currBank = bank.getAttribute("bank_value");
-  if (currBank == NADA) {
-    bank.innerHTML = currClass;
+  let cellType = e.target.getAttribute("cell-type");
+  let currClass = e.target.classList[1];
+
+  if (cellType == 3 || cellType == 4 || cellType == 5) return;
+
+  //------------ check if cell above is not sky
+
+  if (currBank == EMPTY) {
+    let cellIndex = e.target.getAttribute("cell-index").split(",");
     bank.className = currClass;
 
     //firt time switch
-    setBoard(cellIndex, currBank);
+    setBoard(cellIndex, "1");
     //add new new type to bank
     bank.setAttribute("bank_value", cellType);
   }
 
-  if (currBank != NADA) {
-    setBoard(cellIndex, currBank);
-    bank.setAttribute("bank_value", NADA);
-    bank.innerHTML = currClass;
-    bank.className = "";
+  if (currBank == "1") {
+    let cellIndex = e.target.getAttribute("cell-index").split(",");
+    let cellType = e.target.getAttribute("cell-type");
+
+    if (cellType == "2") {
+      //firt time switch
+      setBoard(cellIndex, currBank);
+      //add new new type to bank
+      bank.setAttribute("bank_value", EMPTY);
+    }
   }
 
+  if (currBank == "2") {
+    let cellIndex = e.target.getAttribute("cell-index").split(",");
+    let cellType = e.target.getAttribute("cell-type");
+
+    if (cellType == "1") {
+      //firt time switch
+      setBoard(cellIndex, currBank);
+      //add new new type to bank
+      bank.setAttribute("bank_value", EMPTY);
+    }
+  }
+
+  if (bank.getAttribute("bank_value") == EMPTY) {
+    bank.className = "blank";
+  }
+
+  console.log("new stae " + bank.getAttribute("bank_value"));
   board.innerHTML = "";
   cellsBuilder(board);
+}
+function toolAction2(e) {
+  console.log(e);
+}
+function toolAction3(e) {
+  console.log(e);
 }
 
 /// get cell index  & insert to it new type
 function setBoard(cellIndex, currBank) {
   boardArray[cellIndex[0]][cellIndex[1]] = currBank;
 }
+
+cellsBuilder(board);
 
 //------------------------------------------------------------------------------
 // intro-page off
