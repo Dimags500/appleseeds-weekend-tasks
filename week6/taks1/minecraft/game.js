a6a537ab0e216e436012687b88cacebcaf8d28d7
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-30 16:41:54 +0300

    active buttons added

--- week6/taks1/minecraft/game.js
+++ week6/taks1/minecraft/game.js
@@ -9,13 +9,17 @@ let currTool = "0";
 let tool1 = document.getElementById("tool1");
 let tool2 = document.getElementById("tool2");
 let tool3 = document.getElementById("tool3");
-let tools = [tool1, tool2, tool3].forEach((item) =>
-  item.addEventListener("click", setTool)
-);
+let tools = [tool1, tool2, tool3];
+tools.forEach((item) => item.addEventListener("click", setTool));
 
 function setTool(e) {
   currTool = e.target.getAttribute("tool-type");
-  console.log(currTool + " curr tool ");
+
+  let tools = [tool1, tool2, tool3];
+  for (let i = 0; i < tools.length; i++) {
+    tools[i].style.backgroundColor = "";
+  }
+  e.target.style.backgroundColor = "red";
 }
 
 function cellsBuilder(board) {
